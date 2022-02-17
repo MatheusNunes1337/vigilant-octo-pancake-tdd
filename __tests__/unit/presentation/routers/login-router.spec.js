aa0cc6c169389760c3e6396da300e9acd5c1abf4
Matheus Nunes <matheus.silva@compasso.com.br>
2022-02-17 10:59:45 -0300

    ensure Login router calls AuthUseCase with correct params

--- __tests__/unit/presentation/routers/login-router.spec.js
+++ __tests__/unit/presentation/routers/login-router.spec.js
@@ -1,14 +1,27 @@
 const MissingParamError = require('../../../../src/presentation/helpers/errors/missing-param-error');
 const LoginRouter = require('../../../../src/presentation/routers/login-router');
 
+const makeSut = () => {
+  class AuthUseCaseSpy {
+    auth(email, password) {
+      this.email = email;
+      this.password = password;
+    }
+  }
+
+  const authUseCaseSpy = new AuthUseCaseSpy();
+  const sut = new LoginRouter(authUseCaseSpy);
+
+  return { sut, authUseCaseSpy };
+};
+
 describe('Given the Login Router', () => {
   describe('When email is not provided', () => {
     let httpRequest;
     let httpResponse;
-    let sut;
+    const { sut } = makeSut();
 
     beforeAll(() => {
-      sut = new LoginRouter();
       httpRequest = {
         body: {
           password: 'any_password',
@@ -29,10 +42,9 @@ describe('Given the Login Router', () => {
   describe('When password is not provided', () => {
     let httpRequest;
     let httpResponse;
-    let sut;
+    const { sut } = makeSut();
 
     beforeAll(() => {
-      sut = new LoginRouter();
       httpRequest = {
         body: {
           email: 'any_email@mail.com',
@@ -52,7 +64,7 @@ describe('Given the Login Router', () => {
 
   describe('When httpRequest is not provided', () => {
     test('Then it expects to return status code 500', () => {
-      const sut = new LoginRouter();
+      const { sut } = makeSut();
       const httpResponse = sut.route();
       expect(httpResponse.statusCode).toBe(500);
     });
@@ -60,9 +72,32 @@ describe('Given the Login Router', () => {
 
   describe('When httpRequest has no body', () => {
     test('Then it expects to return status code 500', () => {
-      const sut = new LoginRouter();
+      const { sut } = makeSut();
       const httpResponse = sut.route({});
       expect(httpResponse.statusCode).toBe(500);
+    });
+  });
+
+  describe('When it calls AuthUseCase with correct params', () => {
+    const { sut, authUseCaseSpy } = makeSut();
+    let httpRequest;
+
+    beforeAll(() => {
+      httpRequest = {
+        body: {
+          email: 'any_email@mail.com',
+          password: 'any_password',
+        },
+      };
+      sut.route(httpRequest);
+    });
+
+    test('Then it expects the AuthUseCase email to be http request email', () => {
+      expect(authUseCaseSpy.email).toBe(httpRequest.body.email);
+    });
+
+    test('Then it expects the AuthUseCase password to be http request password', () => {
+      expect(authUseCaseSpy.password).toBe(httpRequest.body.password);
     });
   });
 });
